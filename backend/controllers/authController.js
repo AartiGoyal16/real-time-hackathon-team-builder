f142e75a09f6e0e23c6374220320a7c2d42d27e5
Aarti Goyal <aartigoyal1122006@gmail.com>
2026-03-26 15:18:19 +0530

    fix cross-domain cookies

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -44,11 +44,11 @@ exports.loginUser=async (req,res)=>{
             expiresIn:"7d"
         });
 
-        res.cookie("token",token,{
-            httpOnly:true,
-            secure:false,
-            sameSite:"lax",
-            maxAge:7*24*60*60*1000,
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
         res.status(200).json({
